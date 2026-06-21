f8834fd86502ca4b47f7b02f47a3f16d7c7958ac
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-21 17:33:29 +0300

    Add blogs page

---
+++ blogs-app/app/blogs/page.tsx
@@ -0,0 +1,27 @@
+import { getBlogs } from "../services/blogs";
+
+const Blogs = () => {
+  const blogs = getBlogs();
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            <h3>{blog.title}</h3>
+            <p>
+              By <em>{blog.author}</em> — {blog.likes}{" "}
+              {blog.likes === 1 ? "like" : "likes"}
+            </p>
+            <p>
+              <a href={blog.url}>{blog.url}</a>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Blogs;
