fd8525af7f61cef744eedb90d84913dab9d1a43b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-21 17:24:34 +0300

    Add blogs app home page

--- blogs-app/app/page.tsx
+++ blogs-app/app/page.tsx
@@ -1,3 +1,21 @@
-const Home = () => <div>Hello, World!</div>;
+const Home = () => (
+  <div>
+    <h2>Blogs App</h2>
+    <p>
+      An blogs app created for the exercises in the{" "}
+      <a href="https://courses.mooc.fi/org/uh-cs/courses/full-stack-open-nextjs">
+        Full Stack Open Next.js course
+      </a>
+      .
+    </p>
+    <p>
+      View the{" "}
+      <a href="https://github.com/aarnif/fullstack-open-part-14-nextjs">
+        source code on GitHub
+      </a>
+      .
+    </p>
+  </div>
+);
 
 export default Home;
