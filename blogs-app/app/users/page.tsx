29ab853de2d36ce163a732dff7f6c3e5496bd521
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-28 12:52:17 +0300

    Update users page styles

--- blogs-app/app/users/page.tsx
+++ blogs-app/app/users/page.tsx
@@ -1,16 +1,16 @@
-import Link from "next/link";
 import { getUsers } from "@/app/services/users";
+import NavLink from "@/app/components/NavLink";
 
 const Users = async () => {
   const users = await getUsers();
 
   return (
     <div>
-      <h2>Users</h2>
-      <ul>
+      <h2 className="text-2xl font-bold mb-4">Users</h2>
+      <ul className="flex flex-col gap-2">
         {users.map((user) => (
-          <li key={user.id}>
-            <Link href={`/users/${user.username}`}>{user.name}</Link>
+          <li key={user.id} className="text-lg font-medium">
+            <NavLink href={`/users/${user.username}`}>{user.name}</NavLink>
           </li>
         ))}
       </ul>
