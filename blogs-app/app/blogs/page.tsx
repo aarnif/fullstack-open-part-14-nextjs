5c5bc3f4837ef03638a489aa90f0e51dd82b4d4b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-28 12:47:26 +0300

    Update blogs page styles and remove blog URL link

--- blogs-app/app/blogs/page.tsx
+++ blogs-app/app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { getBlogs } from "@/app/services/blogs";
 import { filterBlogs } from "@/app/actions/blogs";
+import Button from "@/app/components/Button";
 
 const Blogs = async ({
   searchParams,
@@ -12,29 +13,34 @@ const Blogs = async ({
 
   return (
     <div>
-      <h2>Blogs</h2>
-      <form action={filterBlogs}>
+      <h2 className="text-2xl font-bold mb-4">Blogs</h2>
+      <form action={filterBlogs} className="max-w-100 flex gap-2">
         <input
+          className="grow rounded p-2 dark:bg-slate-800 bg-slate-200 text-slate-700 dark:text-white placeholder:text-slate-500"
           type="search"
           name="query"
           defaultValue={filter}
           placeholder="Search blogs by title..."
         />
-        <button type="submit">Search</button>
+        <Button type="submit">Search</Button>
       </form>
-      <ul>
+      <ul className="mt-4 flex flex-col gap-2">
         {blogs.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blogs/${blog.id}`}>
-              <h3>{blog.title}</h3>
+          <li
+            key={blog.id}
+            className="rounded dark:even:bg-slate-900 even:bg-slate-100 hover:bg-slate-200 dark:hover:bg-slate-800 cursor-pointer transition-colors duration-200"
+          >
+            <Link
+              href={`/blogs/${blog.id}`}
+              className="block p-4 cursor-pointer"
+            >
+              <h3 className="text-xl font-bold mb-1">{blog.title}</h3>
+              <p className="mb-3">
+                By <em>{blog.author}</em> — {blog.likes}{" "}
+                {blog.likes === 1 ? "like" : "likes"}
+              </p>
+              <p>{blog.url}</p>
             </Link>
-            <p>
-              By <em>{blog.author}</em> — {blog.likes}{" "}
-              {blog.likes === 1 ? "like" : "likes"}
-            </p>
-            <p>
-              <a href={blog.url}>{blog.url}</a>
-            </p>
           </li>
         ))}
       </ul>
