69640e91338eba32fe85caf426f95fdeb81ef553
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-29 18:15:21 +0300

    Add getUsersReadingList function to users service

--- blogs-app/app/services/users.ts
+++ blogs-app/app/services/users.ts
@@ -1,6 +1,6 @@
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
-import { users } from "@/db/schema";
+import { users, readingList } from "@/db/schema";
 
 export const getUsers = async () => db.query.users.findMany();
 
@@ -8,4 +8,12 @@ export const getUserWithBlogs = async (username: string) =>
   db.query.users.findFirst({
     where: eq(users.username, username),
     with: { blogs: true },
+  });
+
+export const getUsersReadingList = async (userId: number) =>
+  db.query.readingList.findMany({
+    where: eq(readingList.userId, userId),
+    with: {
+      blog: true,
+    },
   });
