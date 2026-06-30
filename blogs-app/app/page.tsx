2aafc3ec52077eb1b8e5ee3b6a0a6365e0a4c4f3
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-30 16:15:05 +0300

    Add static homepage from Markdown

--- blogs-app/app/page.tsx
+++ blogs-app/app/page.tsx
@@ -1,26 +1,8 @@
+import Homepage from "./homepage.mdx";
+
 const Home = () => (
-  <div>
-    <h2 className="text-2xl font-bold mb-4">Blogs App</h2>
-    <p className="mb-2">
-      An blogs app created for the exercises in the{" "}
-      <a
-        href="https://courses.mooc.fi/org/uh-cs/courses/full-stack-open-nextjs"
-        className="text-blue-500 hover:underline"
-      >
-        Full Stack Open Next.js course
-      </a>
-      .
-    </p>
-    <p>
-      View the{" "}
-      <a
-        href="https://github.com/aarnif/fullstack-open-part-14-nextjs"
-        className="text-blue-500 hover:underline"
-      >
-        source code on GitHub
-      </a>
-      .
-    </p>
+  <div className="markdown">
+    <Homepage />
   </div>
 );
 
